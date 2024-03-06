12ef127acfed6aebac9aaa8d9274c035aee2916d
toot_hzf <toot@tootzoe.com>
2024-03-06 21:47:38 +0700

    Initial Commit

---
+++ SrcManipulate/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  SrcManipulate
+//
+//  Created by thor on 6/3/24
+//  
+//
+//  Email: toot@tootzoe.com  Tel: +855 69325538 
+//
+//
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SrcManipulate/SrcManipulateApp.swift
@@ -0,0 +1,23 @@
+//
+//  SrcManipulateApp.swift
+//  SrcManipulate
+//
+//  Created by thor on 6/3/24
+//  
+//
+//  Email: toot@tootzoe.com  Tel: +855 69325538 
+//
+//
+
+
+
+import SwiftUI
+
+@main
+struct SrcManipulateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
